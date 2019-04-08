f46f8cc62ba236f413b7249d526e344dfbd9b245
Rayleighomega <>
2019-04-08 10:22:48 +0200

    FASE 1 Crea una taula (char[]) amb el teu nom on cada posició correspongui a una lletra. Fes un bucle que recorri aquesta taula i mostri per consola cadascuna de les lletres.

---
+++ src/letras/java/Main.java
@@ -0,0 +1,14 @@
+package letras.java;
+
+public class Main {
+
+    public static void main(String[] args)
+    {
+        char[] letras = {'Á','L','V','A','R','O'};
+
+        for (char c : letras)
+        {
+            System.out.println(c);
+        }
+    }
+}
