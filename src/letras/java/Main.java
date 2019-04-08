9318236caa43a296d23b9bee8aa805f976e0dd1f
Rayleighomega <>
2019-04-08 10:54:05 +0200

    FASE 2 Canvia la taula per una llista (List<Character>) Al bucle, si la lletra és una vocal imprimeix a la consola: ‘VOCAL’. Sinó, imprimeix: ‘CONSONTANT’. Si trobes un numero, mostra per pantalla: ‘Els noms de persones no contenen números!’.

--- src/letras/java/Main.java
+++ src/letras/java/Main.java
@@ -1,14 +1,43 @@
 package letras.java;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Main {
 
     public static void main(String[] args)
     {
-        char[] letras = {'Á','L','V','A','R','O'};
+        List <Character> letras = new ArrayList<>();
+
+        letras.add('Á');
+        letras.add('L');
+        letras.add('V');
+        letras.add('A');
+        letras.add('R');
+        letras.add('O');
 
         for (char c : letras)
         {
-            System.out.println(c);
+            if (
+                   c == 'Á' || c == 'A' || c == 'a' || c == 'á'
+                || c == 'É' || c == 'E' || c == 'e' || c == 'é'
+                || c == 'I' || c == 'Í' || c == 'i' || c == 'í'
+                || c == 'Ó' || c == 'O' || c == 'o' || c == 'ó'
+                || c == 'U' || c == 'Ú' || c == 'u' || c =='ú' )
+
+            {
+                System.out.println("VOCAL");
+            }
+            else if(c == '1' || c == '2' || c == '3' || c == '4' || c == '5' || c == '6' || c == '7' || c == '8' || c == '9' || c == '0')
+            {
+                System.out.println("Els noms de persones no contenen números!");
+            }
+            else
+            {
+                System.out.println("CONSONANTE");
+            }
+
+
         }
     }
 }
