8e86bb9f258f44be79626f0a57b64490f95b96c3
Rayleighomega <>
2019-04-10 11:08:28 +0200

    Fix char array to list

--- src/letras/java/Main.java
+++ src/letras/java/Main.java
@@ -7,28 +7,31 @@ public class Main {
 
     public static void main(String[] args)
     {
-        List <Character> letras = new ArrayList<>();
+        List<Character> letras = new ArrayList<Character>();
 
-        letras.add('Á');
+        letras.add('A');
         letras.add('L');
         letras.add('V');
         letras.add('A');
         letras.add('R');
         letras.add('O');
 
+
+        //Input check
+        //-------------------------------------------------------------
         for (char c : letras)
         {
             if (
-                   c == 'Á' || c == 'A' || c == 'a' || c == 'á'
-                || c == 'É' || c == 'E' || c == 'e' || c == 'é'
-                || c == 'I' || c == 'Í' || c == 'i' || c == 'í'
-                || c == 'Ó' || c == 'O' || c == 'o' || c == 'ó'
-                || c == 'U' || c == 'Ú' || c == 'u' || c =='ú' )
+                    c == 'Á' || c == 'A' || c == 'a' || c == 'á'
+                            || c == 'É' || c == 'E' || c == 'e' || c == 'é'
+                            || c == 'I' || c == 'Í' || c == 'i' || c == 'í'
+                            || c == 'Ó' || c == 'O' || c == 'o' || c == 'ó'
+                            || c == 'U' || c == 'Ú' || c == 'u' || c == 'ú')
 
             {
                 System.out.println("VOCAL");
             }
-            else if(c == '1' || c == '2' || c == '3' || c == '4' || c == '5' || c == '6' || c == '7' || c == '8' || c == '9' || c == '0')
+            else if (c == '1' || c == '2' || c == '3' || c == '4' || c == '5' || c == '6' || c == '7' || c == '8' || c == '9' || c == '0')
             {
                 System.out.println("Els noms de persones no contenen números!");
             }
@@ -39,5 +42,7 @@ public class Main {
 
 
         }
+
     }
+
 }
